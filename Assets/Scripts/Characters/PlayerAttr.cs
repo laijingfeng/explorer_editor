b19629fe1926e1235a56e768d8f4d47a19220b30
laijingfeng <337292616@qq.com>
2016-01-19 22:42:31 +0800

    update

--- Assets/Scripts/Characters/PlayerAttr.cs
+++ Assets/Scripts/Characters/PlayerAttr.cs
@@ -2,14 +2,15 @@
 using System.Collections;
 
 /// <summary>
-/// 玩家属性
+/// <para>玩家属性</para>
+/// <para>为了方便编辑器调试，变量都设置Public</para>
 /// </summary>
 public class PlayerAttr : SingletonMono<PlayerAttr>
 {
     /// <summary>
     /// 血量
     /// </summary>
-    private int m_iBlood = 1;
+    public int m_iBlood = 1;
 
     /// <summary>
     /// 血量
@@ -30,7 +31,7 @@ public class PlayerAttr : SingletonMono<PlayerAttr>
     /// <summary>
     /// 连跳次数
     /// </summary>
-    private int m_iJumpCount = 2;
+    public int m_iJumpCount = 2;
 
     /// <summary>
     /// 连跳次数
@@ -46,7 +47,7 @@ public class PlayerAttr : SingletonMono<PlayerAttr>
     /// <summary>
     /// 起跳力
     /// </summary>
-    private float m_fJumpForce = 1000f;
+    public float m_fJumpForce = 1000f;
 
     /// <summary>
     /// 跳跃力
@@ -62,7 +63,7 @@ public class PlayerAttr : SingletonMono<PlayerAttr>
     /// <summary>
     /// 移动最大速度
     /// </summary>
-    private float m_fMaxSpeed = 5f;
+    public float m_fMaxSpeed = 5f;
 
     /// <summary>
     /// 移动最大速度
@@ -78,7 +79,7 @@ public class PlayerAttr : SingletonMono<PlayerAttr>
     /// <summary>
     /// 移动力
     /// </summary>
-    private float m_fMoveForce = 365f;
+    public float m_fMoveForce = 365f;
 
     /// <summary>
     /// 移动力
@@ -91,8 +92,41 @@ public class PlayerAttr : SingletonMono<PlayerAttr>
         }
     }
 
-    public void Init()
-    {
+    /// <summary>
+    /// 属性变化
+    /// </summary>
+    public delegate void OnAttrChange();
 
+    /// <summary>
+    /// 属性变化
+    /// </summary>
+    public OnAttrChange onAttrChange;
+
+    /// <summary>
+    /// 初始化
+    /// </summary>
+    /// <param name="iBlood">血量</param>
+    /// <param name="iJumpCount">连跳次数</param>
+    /// <param name="attrChange">属性变化通知</param>
+    public void Init(int iBlood, int iJumpCount, OnAttrChange attrChange)
+    {
+        m_iBlood = iBlood;
+        m_iJumpCount = iJumpCount;
+
+        onAttrChange -= attrChange;
+        onAttrChange += attrChange;
+
+        PostChange();
+    }
+
+    /// <summary>
+    /// 通知变化
+    /// </summary>
+    private void PostChange()
+    {
+        if (onAttrChange != null)
+        {
+            onAttrChange();
+        }
     }
 }
